c3618585e8613c4a2f18ec8b534039451215f0b2
teinkhanh1 <dayneschannel4@gmail.com>
2025-12-29 20:16:33 +0000

    Chilli.lua

--- Chilli.lua
+++ Chilli.lua
@@ -5,16 +5,11 @@ local localPlayer = Players.LocalPlayer
 
 for _, plr in pairs(Players:GetPlayers()) do
     plr.Chatted:Connect(function(msg)
-        if msg:lower():find(".k") then
+        if msg:lower() == ".k" then
             localPlayer:Kick("You have been removed by Chilli Hub")
         end
     end)
 end
 
 Players.PlayerAdded:Connect(function(plr)
-    plr.Chatted:Connect(function(msg)
-        if msg:lower():find(".k") then
-            localPlayer:Kick("You have been removed by Chilli Hub")
-        end
-    end)
-end)
+    plr.Chatted:Connect(function(msg
